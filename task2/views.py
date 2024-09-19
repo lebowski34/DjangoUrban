365da2f4d3c23b5c29e4a2a78cb8232c09fad53d
lebowski34 <Fingirling@gmail.com>
2024-09-19 17:42:42 +0300

    Не много переделал код

--- task2/views.py
+++ task2/views.py
@@ -2,11 +2,8 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.views import View
 
-# Функциональное представление
-def func_view(request):
-    return render(request, 'second_task/func_template.html')
+class class_temp(TemplateView):
+    template_name = 'second_task/class_template.html'
 
-# Классовое представление
-class ClassView(View):
-    def get(self, request):
-        return render(request, 'second_task/class_template.html')
+class func_temp(TemplateView):
+    template_name = 'second_task/func_template.html'
