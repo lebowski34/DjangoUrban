d7edd748f8ef0284c9f6a778d5811b988e5e2e60
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-22 12:46:09 +0300

    Update views.py

--- task2/views.py
+++ task2/views.py
@@ -2,8 +2,10 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.views import View
 
+# Классовое представление для class_template.html
 class class_temp(TemplateView):
     template_name = 'second_task/class_template.html'
 
-class func_temp(TemplateView):
-    template_name = 'second_task/func_template.html'
+# Функциональное представление для func_template.html
+def func_temp(request):
+    return render(request, 'second_task/func_template.html')
